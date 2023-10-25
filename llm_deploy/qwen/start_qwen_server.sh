119e856658d06724297a1736fd03914b99704944
ppj <pengju.pan@nio.com>
2023-10-26 00:18:21 +0800

    feature:修改qwen dockerfile

--- llm_deploy/qwen/start_qwen_server.sh
+++ llm_deploy/qwen/start_qwen_server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-python -m vllm.entrypoints.openai.api_server \
+python3.9 -m vllm.entrypoints.openai.api_server \
     --model  Qwen/Qwen-7B-Chat\
     --tokenizer Qwen/Qwen-7B-Chat\
     --trust-remote-code  \
