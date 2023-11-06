2ce20ce551d4b0cea199b3dadb687c41f4ec5710
ppj <pengju.pan@nio.com>
2023-11-06 14:33:16 +0800

    feature: 修改qwenfile

---
+++ api_test/test_qwen.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2023/10/27 12:00 下午
+# @Author  : ppj
+# @File    : test_qwen.py
+# @Software: PyCharm
--- llm_deploy/qwen/start_qwen_server.sh
+++ llm_deploy/qwen/start_qwen_server.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 python3.9 -m vllm.entrypoints.openai.api_server \
-    --model  Qwen/Qwen-7B-Chat\
-    --tokenizer Qwen/Qwen-7B-Chat\
-    --trust-remote-code  \
-    --tensor-parallel-size 1
+    --model  Qwen/Qwen-14B-Chat\
+    --tokenizer Qwen/Qwen-14B-Chat\
+    --trust-remote-code       \
+    --tensor-parallel-size 1   \
+    --tokenizer-mode  auto    \
+    --gpu-memory-utilization  0.8 \
+    --port 8000     \
+    --host 0.0.0.0
