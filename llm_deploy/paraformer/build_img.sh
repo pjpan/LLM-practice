168956045fe34cb21fc01a39a6c8a42efc7c8877
ppj <pengju.pan@nio.com>
2024-01-29 14:37:46 +0800

    feature:add sh

---
+++ llm_deploy/paraformer/build_img.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo docker build . -f Dockerfile.server -t pe-hrb.pe-hrb.nioint.com/ad/triton-paraformer-server:23.03
---
+++ llm_deploy/paraformer/push_img.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo docker push pe-hrb.pe-hrb.nioint.com/ad/triton-paraformer-server:23.03
